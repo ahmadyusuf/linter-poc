d8e1528aa68e6c5ddba72767d3c686d68297b90c
Ahmad Yusuf <ahmadyusuf@nbnco.com.au>
2025-10-06 11:34:57 +1100

    first commit

--- sample.py
+++ sample.py
@@ -4,24 +4,20 @@ import os, sys
 
 
 vairableOne=1
-# Function with spacing and naming issues
 def testFunction( x, y ):
   z= x+y
   print("Sum is",z)
   return z
 
-# Unused variable
 unused_var = 42
 
-# Bad indentation
 if True:
   print("This line is fine")
-   print("This line has bad indentation")
+  print("This line has bad indentation")
 
-# Long line for Black to flag
-long_line = "This is a very long line that will exceed the 88 character limit that Black enforces in default configuration"
+long_line = "This is a very long line that will exceed the 88 character \
+limit that Black enforces in default configuration"
 
-# Correct function for comparison
 def add_numbers(a: int, b: int) -> int:
     """Adds two numbers."""
     return a + b
