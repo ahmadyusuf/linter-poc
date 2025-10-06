61601d5bbd37ca068afc92055973a261247c4c1c
Ahmad Yusuf <ahmadyusuf@nbnco.com.au>
2025-10-06 11:52:52 +1100

    first commit

--- sample.py
+++ sample.py
@@ -1,28 +1,32 @@
 # Intentional linting issues for testing
 
-#import os, sys
+# import os, sys
 
 
-vairableOne=1
-def testFunction( x, y ):
-  z= x+y
-  print("Sum is",z)
-  return z
+vairableOne = 1
+
+
+def testFunction(x, y):
+    z = x + y
+    print("Sum is", z)
+    return z
+
 
 unused_var = 42
 
 if True:
-  print("This line is fine")
-  print("This line has bad indentation")
+    print("This line is fine")
+    print("This line has bad indentation")
 
 long_line = "This is a very long line that will exceed the 88 character \
 limit that Black enforces in default configuration"
+
 
 def add_numbers(a: int, b: int) -> int:
     """Adds two numbers."""
     return a + b
 
-# Missing docstring
-def subtract(a,b):
-    return a-b
 
+# Missing docstring
+def subtract(a, b):
+    return a - b
